a4659d01db791d880e60d1bd642a7e63cea20f0e
Sanskar <sanskarmy3dmeta@gmail.com>
2023-03-03 22:42:08 +0530

    Initial Commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+const Home = () => {
+  return (
+    <div>
+      <Head>
+        <title>This is homes</title>
+      </Head>
+      This is Home
+    </div>
+  );
+};
+
+export default Home;
