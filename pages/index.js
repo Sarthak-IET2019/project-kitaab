435eef23cb2524abaa9e6cca0254a2df920ec0c0
Sanskar <sanskarmy3dmeta@gmail.com>
2023-03-04 15:40:01 +0530

    Adding colors

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
 export default function Home() {
   return (
-    // <div className="w-full h-full flex flex-wrap">
-    //   <div className="w-[1/2] h-[400px] bg-primary"></div>
-    //   <div className="w-[1/2] h-[400px] bg-secondary"></div>
-    //   <div className="w-[1/2] h-[400px] bg-accent"></div>
-    //   <div className="w-[1/2] h-[400px] bg-text"></div>
-    // </div>
-    <h1 className="underline text-2xl">This is tailwind</h1>
+    <div className="w-full h-full flex flex-wrap">
+      <div className="w-1/2 h-[400px] bg-primary"></div>
+      <div className="w-1/2 h-[400px] bg-secondary"></div>
+      <div className="w-1/2 h-[400px] bg-accent"></div>
+      <div className="w-1/2 h-[400px] bg-text"></div>
+    </div>
   );
 }
