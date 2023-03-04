b02fec4af8ed77c46558f16d1efb618ace67d964
Abhay <abhaykesarwani51@gmail.com>
2023-03-04 19:09:28 +0530

    Adding auth service

--- pages/_app.js
+++ pages/_app.js
@@ -1,40 +1,15 @@
 import Layout from "@/components/layout";
 import "@/styles/globals.css";
 import { useState } from "react";
-import { createUserWithEmailAndPassword, onAuthStateChange, signOut, signInWithEmailAndPassword } from "firebase/auth";
-import { auth } from 'Firebase';
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from '@/Firebase';
 
 export default function App({ Component, pageProps }) {
-  const [registerEmail, setRegisterEmail] = useState("");
-  const [registerPassword, setRegisterPassword] = useState("");
-  const [loginEmail, setLoginEmail] = useState("");
-  const [loginPassword, setLoginPassword] = useState("");
 
-  const [user, setUser] = useState({});
-  onAuthStateChange(auth, (currentUser) => {
-    setUser(currentUser);
-  })
-  const register = async () => {
-    try {
-      const user = await createUserWithEmailAndPassword(auth, registerEmail, registerPassword);
-      console.log(user);
-    }
-    catch (error) {
-      console.log(error.message);
-    }
-  };
-  const login = async () => {
-    try {
-      const user = await signInWithEmailAndPassword(auth, loginEmail, loginPassword);
-      console.log(user);
-    }
-    catch (error) {
-      console.log(error.message);
-    }
-  };
-  const logout = async () => {
-    await signOut(auth);
-  };
+  // const [user, setUser] = useState({});
+  // onAuthStateChanged(auth, (currentUser) => {
+  //   setUser(currentUser);
+  // })
 
   return (
     <Layout>
---
+++ services/auth.service.js
@@ -0,0 +1,28 @@
+import { auth } from '@/Firebase';
+
+// Function to handle new user registration 
+const HandleUserSignUp = async (email, password) => {
+    try {
+        const user = await createUserWithEmailAndPassword(auth, email, password);
+        console.log(user);
+    }
+    catch (error) {
+        console.log(error.message);
+    }
+};
+
+// Function to handle user signin
+const HandleUserSignIn = async (email, password) => {
+    try {
+        const user = await signInWithEmailAndPassword(auth, email, password);
+        console.log(user);
+    }
+    catch (error) {
+        console.log(error.message);
+    }
+};
+
+// Function to handle user logout
+const HandleUserLogout = async () => {
+    await signOut(auth);
+};
