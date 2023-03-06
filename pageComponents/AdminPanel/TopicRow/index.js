7c7913370b73449903a2a19961dc92e768c17ced
Vivek kumar <57774129+theVivek-kumar@users.noreply.github.com>
2023-03-06 13:59:53 +0530

    feat(add-admin pannel

---
+++ pageComponents/AdminPanel/TopicRow/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const TopicRow = ({data}) => {
+  return (
+    <>
+    <div className="w-full relative flex whitespace-nowrap py-1 px-2 overflow-hidden">
+    <div>
+        
+    </div>
+    
+    <div>
+        
+    </div>
+    </div>
+
+    </>
+  )
+}
+
+export default TopicRow;
